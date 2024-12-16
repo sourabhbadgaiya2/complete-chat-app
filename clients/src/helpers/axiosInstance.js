7c704bbbbcd2fd9252e9dc2f8b861b8f3eb526bb
Sourabh Badgaiya <114412295+sourabhbadgaiya2@users.noreply.github.com>
2024-12-16 23:25:12 +0530

    Update axiosInstance.js

--- clients/src/helpers/axiosInstance.js
+++ clients/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://chatapp-r106.onrender.com " || "http://localhost:5000",
+  baseURL: "https://chatapp-r106.onrender.com",
 });
 
 export default axiosInstance;
